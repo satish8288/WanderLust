5eb7b87168eac9d1333a0d76e3307621930e6f97
Satish <satishbhardwaj8288@gmail.com>
2026-01-22 22:52:20 +0530

    add owner property to listing

--- models/listing.js
+++ models/listing.js
@@ -26,6 +26,10 @@ const listingSchema = new Schema({
       ref: "Review",
     },
   ],
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
 });
 
 //Middleware to delete all reviews of a listing

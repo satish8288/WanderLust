00a7edaef26eed4d5820fa336d78bc2aaf5b1941
Satish <satishbhardwaj8288@gmail.com>
2026-02-24 22:56:26 +0530

    refactoring

--- routes/listings.js
+++ routes/listings.js
@@ -22,27 +22,6 @@ router
     wrapAsync(handleImageUpload),
     wrapAsync(listingControllers.createListing)
   );
-// .post(
-//   upload.single("listing[image]"),
-//   wrapAsync(async (req, res) => {
-//     const result = await new Promise((resolve, reject) => {
-//       cloudinary.uploader
-//         .upload_stream({ folder: "wanderlust_DEV" }, (error, result) => {
-//           if (error) reject(error);
-//           else resolve(result);
-//         })
-//         .end(req.file.buffer);
-//     });
-//     // res.send(req.file);
-//     res.json({
-//       imageUrl: result.secure_url,
-//       public_id: result.public_id,
-//     });
-
-// console.log(req.body);
-// res.send(req.file);
-//   })
-// );
 
 //New Route
 router.get("/new", isLoggedIn, listingControllers.renderNewForm);
